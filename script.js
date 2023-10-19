5a37fac6906e63620a613677462a4548867f67ae
NDMESSA <140191122+NDMESSA@users.noreply.github.com>
2023-10-20 01:44:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,73 +20,56 @@ let calcScrollValue = () => {
   window.onscroll = calcScrollValue;
   window.onload = calcScrollValue;
 
+
   var kontaktContainer = document.getElementById("kontaktContainer");
-      if (kontaktContainer) {
-        kontaktContainer.addEventListener("click", function () {
-          var anchor = document.querySelector("[data-scroll-to='page8']");
-          if (anchor) {
-            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-          }
-        });
+  if (kontaktContainer) {
+    kontaktContainer.addEventListener("click", function () {
+      var anchor = document.querySelector("[data-scroll-to='page71Container']");
+      if (anchor) {
+        anchor.scrollIntoView({ block: "start", behavior: "smooth" });
       }
-      
-      var przygotowanieContainer = document.getElementById("przygotowanieContainer");
-      if (przygotowanieContainer) {
-        przygotowanieContainer.addEventListener("click", function () {
-          var anchor = document.querySelector("[data-scroll-to='page2Container']");
-          if (anchor) {
-            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-          }
-        });
+    });
+  }
+  
+  var uslugiContainer = document.getElementById("uslugiContainer");
+  if (uslugiContainer) {
+    uslugiContainer.addEventListener("click", function () {
+      var anchor = document.querySelector("[data-scroll-to='main']");
+      if (anchor) {
+        anchor.scrollIntoView({ block: "start", behavior: "smooth" });
       }
-      
-      var szukanieContainer = document.getElementById("szukanieContainer");
-      if (szukanieContainer) {
-        szukanieContainer.addEventListener("click", function () {
-          var anchor = document.querySelector("[data-scroll-to='page3']");
-          if (anchor) {
-            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-          }
-        });
+    });
+  }
+  
+  var uslugiContainer1 = document.getElementById("uslugiContainer1");
+  if (uslugiContainer1) {
+    uslugiContainer1.addEventListener("click", function () {
+      var anchor = document.querySelector(
+        "[data-scroll-to='text3frameContainer']"
+      );
+      if (anchor) {
+        anchor.scrollIntoView({ block: "start", behavior: "smooth" });
       }
-      
-      var montazContainer = document.getElementById("montazContainer");
-      if (montazContainer) {
-        montazContainer.addEventListener("click", function () {
-          var anchor = document.querySelector("[data-scroll-to='page4Container']");
-          if (anchor) {
-            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-          }
-        });
+    });
+  }
+  
+  var uslugiContainer2 = document.getElementById("uslugiContainer2");
+  if (uslugiContainer2) {
+    uslugiContainer2.addEventListener("click", function () {
+      var anchor = document.querySelector("[data-scroll-to='page42']");
+      if (anchor) {
+        anchor.scrollIntoView({ block: "start", behavior: "smooth" });
       }
-      
-      var dronyContainer = document.getElementById("dronyContainer");
-      if (dronyContainer) {
-        dronyContainer.addEventListener("click", function () {
-          var anchor = document.querySelector("[data-scroll-to='page5Container']");
-          if (anchor) {
-            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-          }
-        });
+    });
+  }
+  
+  var uslugiContainer3 = document.getElementById("uslugiContainer3");
+  if (uslugiContainer3) {
+    uslugiContainer3.addEventListener("click", function () {
+      var anchor = document.querySelector("[data-scroll-to='page51']");
+      if (anchor) {
+        anchor.scrollIntoView({ block: "start", behavior: "smooth" });
       }
-      
-      var zdjeciaContainer = document.getElementById("zdjeciaContainer");
-      if (zdjeciaContainer) {
-        zdjeciaContainer.addEventListener("click", function () {
-          var anchor = document.querySelector("[data-scroll-to='page6Container']");
-          if (anchor) {
-            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-          }
-        });
-      }
-      
-      var marketingContainer = document.getElementById("marketingContainer");
-      if (marketingContainer) {
-        marketingContainer.addEventListener("click", function () {
-          var anchor = document.querySelector("[data-scroll-to='page7Container']");
-          if (anchor) {
-            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-          }
-        });
-      }
- 
+    });
+  }
+  
